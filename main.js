5734af21d41a36854749bcecf5d27ece0e251894
jwwals1 <joe_walsh59@yahoo.com>
2024-01-03 11:31:10 -0600

    added dropDown functions

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
-function dropDown() {
-    let click = document.getElementById('myDropdowns');
+function newDropDown() {
+    let click = document.getElementById('myNewsDropDowns');
+    if (click.style.display === "block") {
+        click.style.display = "none";
+    } else {
+        click.style.display="block";
+    }
+}
+
+function videoDropDown() {
+    let click = document.getElementById('myVideoDropdowns');
+    if (click.style.display === "block") {
+        click.style.display = "none";
+    } else {
+        click.style.display="block";
+    }
+}
+
+function photoDropDown() {
+    let click = document.getElementById('myPhotoDropdowns');
+    if (click.style.display === "block") {
+        click.style.display = "none";
+    } else {
+        click.style.display="block";
+    }
+}
+
+function chatDropDown() {
+    let click = document.getElementById('myChatDropdowns');
     if (click.style.display === "block") {
         click.style.display = "none";
     } else {
